c904ad6f35da044e4cbac4e33a59f1565ac551e3
Mudoker <huuquoc7603@gmail.com>
2024-03-23 00:36:19 +0700

    feat!: Data preprocessing + initial look thorugh

--- scripts/neko.py
+++ scripts/neko.py
@@ -53,15 +53,13 @@ class Neko:
 
     def is_nan(self, data):
         """
-        Returns True if the DataFrame contains NaN values.
+        Check if the DataFrame contains NaN values.
 
         Parameters:
             data (DataFrame): The DataFrame to analyze.
 
         Returns:
-            Array:
-            Index: 0: Column Name.
-            Index: 1: Number of NaN values.
+            String: A message indicating if the DataFrame contains NaN values.
         """
 
         if not isinstance(data, pd.DataFrame):
@@ -79,3 +77,93 @@ class Neko:
             return data.isnull().sum()
         else:
             return "The DataFrame does not contain any NaN values."
+
+    def is_data_in_range(self, data, column, min_val, max_val):
+        """
+        Check if the values in the specified column are within the provided range.
+
+        Parameters:
+            data (DataFrame): The DataFrame to analyze.
+            column (String): The column to analyze.
+            min_val (Float): The minimum value of the range.
+            max_val (Float): The maximum value of the range.
+
+        Returns:
+            Object: A message indicating if the values are within the range.
+        """
+        # Error handling
+        if not isinstance(data, pd.DataFrame):
+            raise TypeError("Input must be a pandas DataFrame.")
+
+        if data.empty:
+            return "The DataFrame is empty."
+
+        if column not in data.columns:
+            return "The column does not exist in the DataFrame."
+
+        if not isinstance(min_val, (int, float)) or not isinstance(
+            max_val, (int, float)
+        ):
+            raise TypeError("min_val and max_val must be of type int or float.")
+
+        if min_val > max_val:
+            raise ValueError("min_val must be less than max_val.")
+
+        if data[column].dtype not in ["int64", "float64"]:
+            return "The column must contain numeric values."
+
+        # Check if the values are within the range
+        valid_values = data[column].between(min_val, max_val)
+
+        if valid_values.all():
+            return {
+                "message": "All values are within the range.",
+                "is_valid": True,
+            }
+        else:
+            return {
+                "message": f"{sum(~valid_values)} invalid values found.",
+                "is_valid": False,
+            }
+
+    def is_data_valid(self, data, column, valid_values):
+        """
+        Check if the values in the specified column are within the provided list of valid values.
+
+        Parameters:
+            data (DataFrame): The DataFrame to analyze.
+            column (String): The column to analyze.
+            valid_values (List): The list of valid values.
+
+        Returns:
+            String: A message indicating if the values are within the list of valid values.
+        """
+        # Error handling
+        if not isinstance(data, pd.DataFrame):
+            raise TypeError("Input must be a pandas DataFrame.")
+
+        if data.empty:
+            return "The DataFrame is empty."
+
+        if column not in data.columns:
+            return "The column does not exist in the DataFrame."
+
+        if not isinstance(valid_values, list):
+            raise TypeError("valid_values must be a list.")
+
+        if data[column].dtype not in ["int64", "float64"]:
+            return "The column must contain numeric values."
+
+        # Check if the values are within the list of valid values
+        valid_values = data[column].isin(valid_values)
+
+        if valid_values.all():
+            return {
+                "message": "All values are within the range.",
+                "is_valid": True,
+            }
+        else:
+            return {
+                "message": f"{sum(~valid_values)} invalid values found.",
+                "is_valid": False,
+            }
